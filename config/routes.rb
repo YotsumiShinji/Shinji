66f09d3b45c6aee66ba97dd5f65413d2d929ea8d
Shinji <syotsumi380@gmail.com>
2024-02-08 21:46:33 +0900

    routes.rb 変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'posts/:post_id/likes' => 'likes#create'
-  get 'posts/:post_id/likes/:id' => 'likes#destroy'
-  resources :posts
+  get 'posts/nomumae' => 'posts#nomumae'
+  get 'posts/nondeirutoki' => 'posts#nondeirutoki'
+  get 'posts/nondaato' => 'posts#nondaato'
 
   resources :posts do
     resources :likes, only: [:create, :destroy]
@@ -12,10 +12,6 @@ Rails.application.routes.draw do
   end
 
   root 'posts#index'
-
-  get 'posts/nomumae' => 'posts#nomumae'
-  get 'posts/nondeirutoki' => 'posts#nondeirutoki'
-  get 'posts/nondaato' => 'posts#nondaato'
 
   resources :perfumes
 
